115b6ec37a8b0bd7b37d2fffe37f82f04fb1e723
Luis Fernando Raga <luis.raga@laika.com.co>
2021-02-21 18:57:28 -0500

    add log, ip ruming

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 )
 
 func main() {
-
+	port := "5000"
 	route := gin.Default()
 	route.GET("/get_user/:id", controllers.HomeHandler)
-	fmt.Println(utils.GetOutboundIP())
-	route.Run(":5000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	fmt.Println("Corriendo en esta ip: " + utils.GetOutboundIP() + ":" + port)
+	route.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
